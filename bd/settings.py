17582ac52fb7563759e45ff8aed9f3d837a0a786
Anthony YOU <spleen85@proton.me>
2025-05-05 18:06:15 +0200

    Change many things to be good

--- bd/settings.py
+++ bd/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'auhtentication',
-    'bd',
+    'authentication',
+    'site',
 ]
 
 MIDDLEWARE = [
--- site/apps.py
+++ site/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BdConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bd'
+    name = 'site'
