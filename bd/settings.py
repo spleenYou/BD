39846dcafad3cfe1871e5777c123d37e409adbff
Anthony YOU <spleen85@proton.me>
2025-05-05 19:50:31 +0200

    Correct allows_hosts problem

--- bd/settings.py
+++ bd/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+print(ALLOWED_HOSTS)
 
 # Application definition
 
