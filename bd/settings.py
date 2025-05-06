ac24dc4f69a3b7c1310344bff1d23286e7d7b2f5
Anthony YOU <spleen85@proton.me>
2025-05-06 16:50:33 +0200

    Test for static files

--- bd/settings.py
+++ bd/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [BASE_DIR / 'staticfiles']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
