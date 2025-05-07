f583a23980e20380d0d16ec929ded746ed48b29f
Anthony YOU <spleen85@proton.me>
2025-05-07 19:23:01 +0200

    Start account page, show personal information

--- bd/urls.py
+++ bd/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('login/', authentication.views.login_page, name='login'),
     path('logout/', authentication.views.logout_page, name='logout'),
     path('signup/', authentication.views.signup_page, name='signup'),
+    path('account/', mes_bds.views.account, name='account'),
 ]
--- mes_bds/views.py
+++ mes_bds/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
@@ -6,3 +7,8 @@ def home(request):
     if request.user.is_authenticated:
         member_list = [request.user.username.capitalize()]
     return render(request, 'mes_bds/index.html', {'member_list': member_list})
+
+
+@login_required
+def account(request):
+    return render(request, 'mes_bds/account.html')
