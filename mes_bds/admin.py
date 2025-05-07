9d344904e31d46a79923a51fcbb988599a13bf46
Anthony YOU <spleen85@proton.me>
2025-05-07 18:08:33 +0200

    Start admin page for "mes_bds"

--- mes_bds/admin.py
+++ mes_bds/admin.py
@@ -1,3 +1,77 @@
 from django.contrib import admin
+from .models import Family, Author, ComicBook, Manga, Book, Library
 
-# Register your models here.
+
+@admin.register(Family)
+class Family(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'user',
+    )
+    list_filter = (
+    )
+    list_display_links = (
+    )
+
+
+@admin.register(Author)
+class Author(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'lastname',
+        'firstname',
+    )
+    list_filter = (
+    )
+    list_display_links = (
+    )
+
+
+@admin.register(ComicBook)
+class ComicBook(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'number',
+        'name',
+    )
+    list_filter = (
+    )
+    list_display_links = (
+    )
+
+
+@admin.register(Manga)
+class Manga(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'number',
+        'name',
+    )
+    list_filter = (
+    )
+    list_display_links = (
+    )
+
+
+@admin.register(Book)
+class Book(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+    )
+    list_filter = (
+    )
+    list_display_links = (
+    )
+
+
+@admin.register(Library)
+class Library(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'user',
+    )
+    list_filter = (
+    )
+    list_display_links = (
+    )
